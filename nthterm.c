6e7ad7422d1f65b048ec9084122ab8707781e4c5
Nitesh-1707 <nk7483684996@gmail.com>
2020-09-21 21:04:20 +0530

    NthTerm

---
+++ nthterm.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c) {
+  //Write your code here.
+  int ans[100];
+  ans[a] = a;
+  ans[b] = b;
+  ans[c] = c;
+
+  ans[n] = ans[n-a]+ans[n-b]+ans[n-c];
+  return ans[n];
+}
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
