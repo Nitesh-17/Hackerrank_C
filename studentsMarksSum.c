3079da074b044a69f78c9ad2cd55965977671594
Nitesh-1707 <nk7483684996@gmail.com>
2020-09-26 16:13:19 +0530

    Easy

---
+++ studentsMarksSum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+  //Write your code here.
+  int sum=0;
+  int i;
+  
+  if (gender=='b'){
+    for( i =0;i<number_of_students;i=i+2){
+      sum = sum + marks[i];
+    
+    }
+  }
+  else
+  for( i =1;i<number_of_students;i=i+2){
+    sum = sum + marks[i];
+  
+  }
+  return sum;
+
+}
